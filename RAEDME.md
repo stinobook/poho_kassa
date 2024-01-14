254cacb1184c4d786bf8abdd0c0c5b6955fc4f83
VandeurenGlenn <vandeurenglenn@gmail.com>
2024-01-14 11:39:27 +0100

    Add README & explain icons

---
+++ RAEDME.md
@@ -0,0 +1,30 @@
+# PoHo
+
+> cash register app
+
+## development
+
+### watch
+
+```sh
+npm run watch
+```
+
+### build
+
+```sh
+npm run build
+```
+
+### serve
+
+```sh
+npm run serve
+```
+
+## powered by
+
+- [lit](https://lit.dev)
+- [material-web](https://material-web.dev/)
+- [lit-elements](https://github.com/VandeurenGlenn/custom-elements/tree/main/packages/lit-elements)
+- [rollup-plugin-material-symbols](https://github.com/vandeurenglenn/rollup-plugin-material-symbols)
--- src/icons.ts
+++ src/icons.ts
@@ -1,4 +1,19 @@
 import { html } from 'lit'
+/**
+ * Inlined icons as svg vs fetching the iconfont
+ * Icons are inlined using the rollup-plugin-material-symbols [see](./config/rollup.config.js)
+ *
+ * Adding an icon
+ * go to https://fonts.google.com/icons
+ * find the name of the desired icon
+ *
+ * add to iconset like
+ * <span name="icon_name">@symbol-icon_name></span>
+ *
+ * use in app
+ * <custom-icon icon="icon_name"></custom-icon>
+ * <custom-icon-button icon="icon_name"></custom-icon-button>
+ */
 export default html`
   <custom-icon-set>
     <template>
