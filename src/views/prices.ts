bef3eedfbcd0b9f1a91ff367f31df34fca2428c8
stinobook <stijn.schepers@me.com>
2024-01-19 14:38:40 +0100

    added category tabs to salesview

--- src/views/prices.ts
+++ src/views/prices.ts
@@ -8,7 +8,7 @@ type priceList = {
   price: number;
 }
 
-let prijslijst: priceList[] = [
+export let prijslijst: priceList[] = [
   // Frisdranken
   { "name": "Cola", "category" : "frisdrank", "vat": 21, "price": 2 },
   { "name": "Cola zero", "category" : "frisdrank", "vat": 21, "price": 2 },
@@ -28,6 +28,34 @@ let prijslijst: priceList[] = [
   { "name": "Omer", "category" : "bieren", "vat": 21, "price": 3 },
   { "name": "Bier v/d maand", "category" : "bieren", "vat": 21, "price": 3 },
   { "name": "Orval", "category" : "bieren", "vat": 21, "price": 4 },
+  // Wijnen-Apero
+  { "name": "Witte Wijn 25cl", "category" : "wijnen-apero", "vat": 21, "price": 4 },
+  { "name": "Rose Wijn 25cl", "category" : "wijnen-apero", "vat": 21, "price": 4 },
+  { "name": "Rode Wijn", "category" : "wijnen-apero", "vat": 21, "price": 4 },
+  { "name": "Zizi", "category" : "wijnen-apero", "vat": 21, "price": 4 },
+  { "name": "Pompierke", "category" : "wijnen-apero", "vat": 21, "price": 4 },
+  { "name": "Limoncello", "category" : "wijnen-apero", "vat": 21, "price": 4 },
+  { "name": "Cava", "category" : "wijnen-apero", "vat": 21, "price": 4 },
+  { "name": "Aperol", "category" : "wijnen-apero", "vat": 21, "price": 5 },
+  // Warme Dranken
+  { "name": "Koffie", "category" : "warme dranken", "vat": 21, "price": 2 },
+  { "name": "Thee", "category" : "warme dranken", "vat": 21, "price": 2 },
+  { "name": "Minuut Soep", "category" : "warme dranken", "vat": 21, "price": 2 },
+  { "name": "Warme Choco", "category" : "warme dranken", "vat": 21, "price": 2 },
+  { "name": "Verse Soep (Winter)", "category" : "warme dranken", "vat": 21, "price": 3 },
+  // Versnaperingen
+  { "name": "Worst", "category" : "versnaperingen", "vat": 21, "price": 2 },
+  { "name": "Chips", "category" : "versnaperingen", "vat": 21, "price": 2 },
+  // Winkel
+  { "name": "Lidgeld", "category" : "winkel", "vat": 21, "price": 120 },
+  { "name": "Extra lid", "category" : "winkel", "vat": 21, "price": 10 },
+  { "name": "Rodiworst", "category" : "winkel", "vat": 21, "price": 3 },
+  { "name": "Zwan", "category" : "winkel", "vat": 21, "price": 3 },
+  { "name": "Klein been", "category" : "winkel", "vat": 21, "price": 2 },
+  { "name": "Groot been", "category" : "winkel", "vat": 21, "price": 3 },
+  { "name": "Poepzakjes", "category" : "winkel", "vat": 21, "price": 12 },
+  { "name": "Limoncello 0.5L", "category" : "winkel", "vat": 21, "price": 25 },
+  { "name": "Limoncello 1L", "category" : "winkel", "vat": 21, "price": 40 },
 ]
 
 @customElement('prices-view')
