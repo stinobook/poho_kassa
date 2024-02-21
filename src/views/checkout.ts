6c01b135a2cadcb965262234fef8135cd8a50496
stinobook <stijn.schepers@me.com>
2024-02-21 18:38:04 +0100

    working on checkout lists

--- src/views/checkout.ts
+++ src/views/checkout.ts
@@ -1,5 +1,6 @@
 import { html, css, LiteElement, property } from '@vandeurenglenn/lite'
 import { customElement } from 'lit/decorators.js'
+import { map } from 'lit/directives/map.js'
 import '@material/web/list/list.js'
 import '@material/web/list/list-item.js'
 import '@material/web/textfield/filled-text-field.js'
@@ -77,8 +78,9 @@ export class CheckoutView extends LiteElement {
         width: 50%;
       }
       .total {
-        box-sizing: border-box;
-        padding: 12px 24px;
+        padding: 12px 12px;
+        border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+        height: 24px;
         width: 100%;
       }
       md-list {
@@ -122,11 +124,10 @@ export class CheckoutView extends LiteElement {
           this.cashExpected += key.paymentAmount
         }
       })
+      console.log(keylessTransactions)
       this.requestRender()
     })
   }
-
-
 
 
   render() {
@@ -185,26 +186,19 @@ export class CheckoutView extends LiteElement {
             <md-list>
               <md-list-item>Winkel</md-list-item>
               <md-divider></md-divider>
-              <md-list-item>
-                <span slot="start">Zwan</span>
-                <span slot="end">3&euro;</span>
-                <span slot="trailing-supporting-text">Payconiq</span>
-              </md-list-item>
-              <md-list-item>
-                <span slot="start">Groot been</span>
-                <span slot="end">3&euro;</span>
-                <span slot="trailing-supporting-text">Payconiq</span>
-              </md-list-item>
-              <md-list-item>
-                <span slot="start">Klein been</span>
-                <span slot="end">3&euro;</span>
-                <span slot="trailing-supporting-text">Cash</span>
-              </md-list-item>
-              <md-list-item>
-                <span slot="start">Rodiworst</span>
-                <span slot="end">3&euro;</span>
-                <span slot="trailing-supporting-text">Cash</span>
-              </md-list-item>
+              ${this.allTransactions
+                ? map(
+                  Object.values(this.allTransactions),
+                  (transaction: Transaction) => 
+                  html`
+                    <md-list-item>
+                    <span slot="start">${transaction.transactionItems }</span>
+                    <span slot="end">${transaction.paymentAmount }</span>
+                    <span slot="trailing-supporting-text">${transaction.paymentMethod}</span>
+                    </md-list-item>
+                  `
+                )
+              : ''}
             </md-list>
             <flex-row center class="total">
               <strong>Totaal:</strong>
@@ -217,30 +211,6 @@ export class CheckoutView extends LiteElement {
             <md-list>
               <md-list-item>Lidgeld</md-list-item>
               <md-divider></md-divider>
-              <md-list-item>
-                <span slot="headline">Lidgeld</span>
-                <span slot="supporting-text">Mary Jane met Robin</span>
-                <span slot="end">120&euro;</span>
-                <span slot="trailing-supporting-text">Payconiq</span>
-              </md-list-item>
-              <md-list-item>
-                <span slot="headline">Extra lid</span>
-                <span slot="supporting-text">Peter Parker</span>
-                <span slot="end">10&euro;</span>
-                <span slot="trailing-supporting-text">Payconiq</span>
-              </md-list-item>
-              <md-list-item>
-                <span slot="headline">Lidgeld</span>
-                <span slot="supporting-text">Carmen Waterslaeghers met Nero</span>
-                <span slot="end">120&euro;</span>
-                <span slot="trailing-supporting-text">Cash</span>
-              </md-list-item>
-              <md-list-item>
-                <span slot="headline">Extra lid</span>
-                <span slot="supporting-text">Xavier Waterslaeghers</span>
-                <span slot="end">10&euro;</span>
-                <span slot="trailing-supporting-text">Cash</span>
-              </md-list-item>
             </md-list>
             <flex-row center class="total">
               <strong>Totaal:</strong>
