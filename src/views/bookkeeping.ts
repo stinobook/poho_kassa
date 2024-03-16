dec5a1dae57a776a6e217a16ee270bd46c0bad50
VandeurenGlenn <vandeurenglenn@gmail.com>
2024-03-16 11:40:59 +0100

    bookkeeping: draft changes

--- src/views/bookkeeping.ts
+++ src/views/bookkeeping.ts
@@ -1,6 +1,4 @@
-import { html, css, LiteElement, property } from '@vandeurenglenn/lite'
-import { customElement } from 'lit/decorators.js'
-import { map } from 'lit/directives/map.js'
+import { html, css, LiteElement, property, customElement } from '@vandeurenglenn/lite'
 import '@material/web/list/list.js'
 import '@material/web/list/list-item.js'
 import '@material/web/textfield/filled-text-field.js'
@@ -8,7 +6,8 @@ import '@vandeurenglenn/flex-elements/row.js'
 import '@vandeurenglenn/flex-elements/container.js'
 import '@vandeurenglenn/flex-elements/column.js'
 import '@material/web/button/filled-button.js'
-import { get, ref, push, getDatabase, child, onChildAdded, onChildRemoved, set } from 'firebase/database'
+import { get, ref, getDatabase } from 'firebase/database'
+import { Sales } from '../types.js'
 
 const formatDate = () => {
   const date = new Date().toLocaleDateString('fr-CA').split('-')
@@ -19,7 +18,7 @@ const formatDate = () => {
 
 @customElement('bookkeeping-view')
 export class BookkeepingView extends LiteElement {
-  @property() accessor books: { [key]: Sales[] } = {}
+  @property() accessor books: { [key: string]: Sales[] } = {}
   static styles = [
     css`
       :host {
@@ -122,7 +121,7 @@ export class BookkeepingView extends LiteElement {
       details[open] summary ~ * {
         animation: open 0.3s ease-in-out;
       }
-      
+
       @keyframes open {
         0% {
           opacity: 0;
@@ -134,15 +133,15 @@ export class BookkeepingView extends LiteElement {
       details summary::-webkit-details-marker {
         display: none;
       }
-      
+
       details summary {
         position: relative;
         cursor: pointer;
         list-style: none;
       }
-      
+
       details summary:after {
-        content: "+";
+        content: '+';
         height: 10px;
         position: absolute;
         font-size: 1.75rem;
@@ -174,8 +173,6 @@ export class BookkeepingView extends LiteElement {
         margin: 12px;
         flex-wrap: wrap;
       }
-      #card-input span {
-      }
       #card-input input {
         padding: 10px 15px;
         font-size: 1rem;
@@ -187,7 +184,7 @@ export class BookkeepingView extends LiteElement {
         margin-left: 24px;
         margin-right: 4px;
       }
-      
+
       #card-input label {
         font-size: 1rem;
         border-top-left-radius: 3px;
@@ -199,39 +196,46 @@ export class BookkeepingView extends LiteElement {
   ]
 
   async connectedCallback() {
-    this.shadowRoot.addEventListener('input', ({ target }: CustomEvent) => {
+    this.shadowRoot.addEventListener('input', ({ detail }: CustomEvent) => {
       // @ts-ignore
-      let event = new CustomEvent('loadBooksInput', { detail: 'loadbooks'})
-      this.loadBooks(event)
+      let event = new CustomEvent('loadBooksInput', { detail: 'loadbooks' })
+
+      if (detail === 'loadbooks') {
+        this.loadBooks()
+      }
     })
   }
 
-  async loadBooks({ detail }: CustomEvent) {
-    if (detail === 'loadbooks') {
-      let fromDate = new Date((this.shadowRoot.querySelector('#fromDate') as HTMLInputElement).value)
-      let toDate = new Date((this.shadowRoot.querySelector('#toDate') as HTMLInputElement).value)
-      const salesDB = ref(getDatabase(), 'sales')
-      let dbData = await get(salesDB).then((snapshot) => {
+  async loadBooks() {
+    let fromDate = new Date((this.shadowRoot.querySelector('#fromDate') as HTMLInputElement).value)
+    let toDate = new Date((this.shadowRoot.querySelector('#toDate') as HTMLInputElement).value)
+    const salesDB = ref(getDatabase(), 'sales')
+    let dbData = await get(salesDB)
+      .then((snapshot) => {
         if (snapshot.exists()) {
           return snapshot.val()
         } else {
-          console.log("No data available");
+          console.log('No data available')
         }
-      }).catch((error) => {
-        console.error(error);
-      });
-      let filteredData = Object.fromEntries(Object.entries(dbData).filter(([key, value]) => new Date(value.date.slice(0, 10)).toISOString() >= fromDate.toISOString() && new Date(value.date.slice(0, 10)).toISOString() <= toDate.toISOString()))
-      this.books = filteredData
-      this.requestRender()
-    }
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+    let filteredData = Object.fromEntries(
+      Object.entries(dbData).filter(
+        ([key, value]) =>
+          new Date(value.date.slice(0, 10)).toISOString() >= fromDate.toISOString() &&
+          new Date(value.date.slice(0, 10)).toISOString() <= toDate.toISOString()
+      )
+    )
+    this.books = filteredData
+    this.requestRender()
   }
 
-
-
-  renderBooks(books = this.books) {
-    return Object.entries(this.books).map(([key, value]) => 
-      books
-      ? html`
+  renderBooks() {
+    return Object.entries(this.books).map(([key, value]) =>
+      this.books
+        ? html`
             <div id="card-main">
               <span class="date">${value.date}</span>
               <div id="card-sub">
@@ -265,26 +269,31 @@ export class BookkeepingView extends LiteElement {
               <div id="card-sub-wide">
                 <span>Payconiq betalingen</span>
                 <div id="card-sub-details">
-                    ${value.transactions.map((transaction) => {
-                      if (transaction.paymentMethod === 'payconiq') {
-                          let items = Object.entries(transaction.transactionItems).map(([key, transactionItem]) =>
-                              html`
-                              <md-list-item>
+                  ${value.transactions.map((transaction) => {
+                    if (transaction.paymentMethod === 'payconiq') {
+                      let items = Object.entries(transaction.transactionItems).map(
+                        ([key, transactionItem]) =>
+                          html`
+                            <md-list-item>
                               <span slot="headline">${transactionItem.description}</span>
                               <span slot="start">${transactionItem.amount} x ${transactionItem.name}</span>
-                              <span slot="end">Eenheid: &euro;${transactionItem.price} / Totaal: &euro;${Number(transactionItem.price) * Number(transactionItem.amount)}</span>
-                              </md-list-item>
-                              `
-                            )
-                            let summary = html`<details><summary>
-                            <span>Transactie van: &euro;</span>
-                            <span>${transaction.paymentAmount}</span>
-                            </summary><md-list>
-                            ${items}
-                            </md-list></details>
-                            `
-                            return [summary]
-                    }})}
+                              <span slot="end"
+                                >Eenheid: &euro;${transactionItem.price} / Totaal:
+                                &euro;${Number(transactionItem.price) * Number(transactionItem.amount)}</span
+                              >
+                            </md-list-item>
+                          `
+                      )
+                      let summary = html`<details>
+                        <summary>
+                          <span>Transactie van: &euro;</span>
+                          <span>${transaction.paymentAmount}</span>
+                        </summary>
+                        <md-list> ${items} </md-list>
+                      </details> `
+                      return [summary]
+                    }
+                  })}
                 </div>
               </div>
               <div id="card-sub-wide">
@@ -293,40 +302,49 @@ export class BookkeepingView extends LiteElement {
                   ${value.transactions.map((transaction) => {
                     let paymentAmount = transaction.paymentAmount
                     let paymentMethod = transaction.paymentMethod
-                    let transactionItemLidgeld = Object.entries(transaction.transactionItems).filter(([key, transactionItem]) => transactionItem.category === 'Lidgeld').map(([key, transactionItem]) =>
-                              html`
-                              <md-list-item>
+                    let transactionItemLidgeld = Object.entries(transaction.transactionItems)
+                      .filter(([key, transactionItem]) => transactionItem.category === 'Lidgeld')
+                      .map(
+                        ([key, transactionItem]) =>
+                          html`
+                            <md-list-item>
                               <span slot="headline">${transactionItem.description}</span>
                               <span slot="start">${transactionItem.name}</span>
                               <span slot="end">&euro;${transactionItem.price}</span>
-                              </md-list-item>
-                              `
-                            )
-                            let summary = html`<details><summary>
-                            <span>${paymentMethod[0].toUpperCase() + paymentMethod.slice(1)}betaling van: &euro;</span>
-                            <span>${paymentAmount}</span>
-                            </summary><md-list>
-                            ${transactionItemLidgeld}
-                            </md-list></details>
-                            `
-                    if (transactionItemLidgeld.length !== 0) {return [summary]}
+                            </md-list-item>
+                          `
+                      )
+                    let summary = html`<details>
+                      <summary>
+                        <span>${paymentMethod[0].toUpperCase() + paymentMethod.slice(1)}betaling van: &euro;</span>
+                        <span>${paymentAmount}</span>
+                      </summary>
+                      <md-list> ${transactionItemLidgeld} </md-list>
+                    </details> `
+                    if (transactionItemLidgeld.length !== 0) {
+                      return [summary]
+                    }
                   })}
                 </div>
               </div>
-            </div> 
-        `
-      : ''
+            </div>
+          `
+        : ''
     )
   }
 
   render() {
     return html`
       <flex-container direction="row">
-      <div id="card-input">
-      <span><label>Van: <input type="date" id="fromDate" value=${formatDate()}></label></span>
-      <span><label>Tot: <input type="date" id="toDate" value=${formatDate()}></label></span>
-      </div>
-      ${this.books ? this.renderBooks() : ''}
+        <div id="card-input">
+          <span
+            ><label>Van: <input type="date" id="fromDate" value=${formatDate()} /></label
+          ></span>
+          <span
+            ><label>Tot: <input type="date" id="toDate" value=${formatDate()} /></label
+          ></span>
+        </div>
+        ${this.books ? this.renderBooks() : ''}
       </flex-container>
     `
   }
